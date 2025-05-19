4d44c8c826ff192ecffe04573877282e87de8f9b
Javier Serrano <javierserranojodral@gmail.com>
2025-05-19 23:52:52 +0200

    Some progress in Project 2

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True)
+
+
+class Bid(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,12 +3,16 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import User, AuctionListing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = AuctionListing.objects.all()
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 
 def login_view(request):
@@ -61,3 +65,23 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+@login_required
+def create_listing(request):
+    if request.method == "GET":
+        return render(request, "auctions/create_listing.html")
+    else:
+        title = request.POST["title"]
+        description = request.POST["description"]
+        starting_bid = request.POST["starting_bid"]
+        image_url = request.POST["image_url"]
+
+        listing = AuctionListing(
+            title=title,
+            description=description,
+            starting_bid=starting_bid,
+            image_url=image_url
+        )
+        listing.save()
+        return HttpResponseRedirect(reverse("index"))
